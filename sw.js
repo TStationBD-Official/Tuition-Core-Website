a660a2ee3928a335f56947606e693675f09203f8
TStationBD <t.station.official@gmail.com>
2026-06-28 18:05:12 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11211822
+    "domain": "3nbf4.com",
+    "zoneId": 11211856
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
